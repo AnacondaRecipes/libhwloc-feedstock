fdb2c61011fad3bb4339d22a0785810bfd21dfe0
Eric Lundby <Eric.Lundby@gmail.com>
2026-06-29 13:01:31 -0600

    Move win cuda to bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,9 +1,18 @@
+set "CUDA_CMAKE_ARGS="
+if not "%cuda_compiler_version%"=="None" (
+  rem hwloc links cudart at build time; point CMake at conda CUDA in BUILD_PREFIX
+  set "CUDA_HOME=%BUILD_PREFIX%\Library"
+  set "CUDA_PATH=%BUILD_PREFIX%\Library"
+  set "CUDA_CMAKE_ARGS=-DHWLOC_WITH_CUDA=ON -DCUDAToolkit_ROOT=%BUILD_PREFIX%"
+)
+
 cmake -G "Ninja" ^
   -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
   -DCMAKE_BUILD_TYPE=Release ^
   -DBUILD_SHARED_LIBS=ON ^
   -DHWLOC_WITH_LIBXML2=ON ^
   -DHWLOC_ENABLE_TESTING=OFF ^
+  %CUDA_CMAKE_ARGS% ^
   %SRC_DIR%\contrib\windows-cmake
 if %ERRORLEVEL% neq 0 exit 1
 
--- recipe/build.sh
+++ recipe/build.sh
@@ -23,35 +23,6 @@ case "${target_platform:-${TARGET_PLATFORM}}" in
           ./configure --prefix=$PREFIX $DISABLES
         fi
         ;;
-    win-*)
-        export CPPFLAGS="$CPPFLAGS -Dputenv=_putenv -Dmktemp=_mktemp -Dopen=_open -Dunlink=_unlink -Dclose=_close -Dstrdup=_strdup"
-        export HWLOC_LDFLAGS="-no-undefined"
-        # Skip failing tests that are skipped on Linux x86_64 and OSX, but not skipped on windows
-        sed -i "s|SUBDIRS += x86||g" tests/hwloc/Makefile.am
-        sed -i "s|-Xlinker --output-def -Xlinker .libs/libhwloc.def||g" hwloc/Makefile.am
-        autoreconf -ivf
-        chmod +x configure
-        if [[ ${cuda_compiler_version} != "None" ]]; then
-          # cudart.lib path differs between CUDA 12.x (Library/lib) and 13.x (Library/lib/x64)
-          if [[ ${cuda_compiler_version} == 12.* ]]; then
-            CUDA_LIBDIR="$BUILD_PREFIX/Library/lib"
-          else
-            CUDA_LIBDIR="$BUILD_PREFIX/Library/lib/x64"
-          fi
-          # hwloc.m4 falls back to header+lib autoconf probes when pkg-config
-          # cannot find cuda-$VERSION.pc (no .pc file shipped on Windows).
-          # Inject conda CUDA paths via CPPFLAGS / LDFLAGS so the probe succeeds.
-          export CPPFLAGS="$CPPFLAGS -I$BUILD_PREFIX/Library/include"
-          export LDFLAGS="$LDFLAGS -L$CUDA_LIBDIR"
-          ./configure --enable-cuda --prefix="$PREFIX" --libdir="$PREFIX/lib" \
-            --disable-cairo --disable-opencl --disable-gl --disable-libudev \
-            --disable-static || (cat config.log; false)
-        else
-          ./configure --prefix="$PREFIX" --libdir="$PREFIX/lib" $DISABLES --disable-static || (cat config.log; false)
-        fi
-        patch_libtool
-        make V=1
-        ;;
 esac
 
 make -j${CPU_COUNT} V=1
